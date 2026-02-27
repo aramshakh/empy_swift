798849eaad83be95133af48d02c647cc05107771
aramshakh <leadmakingadv@gmail.com>
2026-02-27 17:20:44 +0400

    Initial Commit

---
+++ Empy_Swift/Empy_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empy_SwiftApp.swift
+//  Empy_Swift
+//
+//  Created by Aram on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Empy_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
