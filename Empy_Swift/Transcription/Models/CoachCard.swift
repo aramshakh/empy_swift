7120ca6731bb8530f6a0336363e9920cddb5ed40
Aram Shakhnazaryan <aramshakh@gmail.com>
2026-03-03 21:48:43 +0000

    feat(T15): coach cards overlay with warnings/tips/insights

---
+++ Empy_Swift/Transcription/Models/CoachCard.swift
@@ -0,0 +1,40 @@
+import Foundation
+import SwiftUI
+
+enum CoachCardType {
+    case warning
+    case tip
+    case insight
+    
+    var icon: String {
+        switch self {
+        case .warning: return "exclamationmark.triangle.fill"
+        case .tip: return "lightbulb.fill"
+        case .insight: return "star.fill"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .warning: return .red
+        case .tip: return .blue
+        case .insight: return .green
+        }
+    }
+}
+
+struct CoachCard: Identifiable {
+    let id: UUID
+    let type: CoachCardType
+    let title: String
+    let message: String
+    let timestamp: Date
+    
+    init(type: CoachCardType, title: String, message: String) {
+        self.id = UUID()
+        self.type = type
+        self.title = title
+        self.message = message
+        self.timestamp = Date()
+    }
+}
